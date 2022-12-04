c5451bf16188a517f61d6401bc5b6fb713eac429
Shivansh-Garg <79331252+Shivansh-Garg@users.noreply.github.com>
2022-12-04 18:12:57 +0530

    Update data_analysis_code.cpp
    addition of adam optimization

--- data_analysis/data_analysis_code.cpp
+++ data_analysis/data_analysis_code.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+
 float cal_mean(int arr[] , int ei){
     float sum =0;
     for(int i=0;i<=ei;i++){
@@ -92,6 +93,43 @@ int data[] = {0,2,1,22 ,2 ,1 ,3 ,5 ,9 ,15 ,8 ,10 ,10 ,11 ,10 ,14 ,20 ,25 ,27 ,58
     cout<<endl;
     cout<<endl;
 
+    //mean normalization
+    int min_ele = 999999;
+    int max_ele = -1;
+    for(int i=0;i<n;i++){
+        if(min_ele > data[i]){
+            min_ele = data[i];
+        }
+        if(max_ele < data[i]){
+            max_ele = data[i];
+        }
+    }
+    float range = (float)max_ele - (float)min_ele;
+    vector<float> normalized_arr;
+    cout<<endl;
+    cout<<"data after normalization is as follows: "<<endl;
+    cout<<endl;
+    for(int i=0;i<n;i++){
+        float temp = (abs)(data[i] - average)/range;
+        normalized_arr.push_back(temp);
+        cout<<temp<<"  ";
+    }
+    cout<<endl;
+
+    //adam optimization
+    float alpha = 1;
+    float epsilon = 1;
+    float val;
+    vector<float>adamOpt;
+    cout<<endl;
+    cout<<"adam optimization of the above data is as follows: "<<endl;
+    for(int i =1; i<laplacian.size(); i++)
+    {
+        val = (data[i] - alpha * arr_gradient[i-1])/(sqrt((laplacian[i]*laplacian[i]) + epsilon));
+        adamOpt.push_back(val);
+        cout<<val<<" ";
+    }
+    cout<<endl;
 
 return 0;
 }
